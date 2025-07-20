8e5c878a8dd718caa7be81eb0cd519dfdddbd116
Igor de Lima Nunes <igorlnunes@gmail.com>
2025-07-20 19:28:32 -0300

    fix: add dotenv

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,6 @@
-declare const SB_URL: string;
-declare const SB_KEY: string;
-
 export const environment = {
     production: false,
-    supabaseUrl: SB_URL,
-    supabaseKey: SB_KEY,
+    supabaseUrl: '',
+    supabaseKey: '',
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,4 @@
 declare const SB_URL: string;
 declare const SB_KEY: string;
 
-export const environment = {
-    production: true,
-    supabaseUrl: SB_URL,
-    supabaseKey: SB_KEY,
-};
+export const environment = {};
