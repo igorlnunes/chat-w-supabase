004780f36b1532d48971d8de7072cfacd82434d8
Igor de Lima Nunes <igorlnunes@gmail.com>
2025-07-20 17:59:23 -0300

    feat: integrate with firebase & supabase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://sbqdkgjaeczdrvaajemc.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicWRrZ2phZWN6ZHJ2YWFqZW1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5NDIxNzIsImV4cCI6MjA2ODUxODE3Mn0.ay5xUBzgX5QBJ1GKglvmzCpe54zIdUJJzJ9RNcmDIk8'
+};
+
